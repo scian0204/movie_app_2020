7a7d5baf51ef85a03d2a65e41f17bc687343858c
scian0204 <scian0204@gmail.com>
2020-09-18 12:13:18 +0900

    edit readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+function Food({fav}) {
+  return (
+    <h3>I Love {fav}</h3>
+  );
+}
+
 function App() {
   // return <div className="App" />
   return (
     <div>
       <h1>hello world!</h1>
+      <Food fav="kimchi" />
+      <Food fav="pizza" />
+      <Food fav="chicken" />
+      <Food fav="dumpling" />
     </div>
   );
 }
